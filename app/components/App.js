8c7fb15921be43f788ded5daae4c93ec37163d67
3lv27 <elvin.gm27@gmail.com>
2018-01-28 12:45:16 +0100

    progress: added react router

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import {
+    BrowserRouter,
+    Route,
+    Switch
+} from 'react-router-dom';
 
-import Popular from './Popular'
+import Popular from './Popular';
+import Home from './Home';
+import Battle from './Battle';
+import Nav from './Nav';
+
 
 class App extends Component {
     render() {
         return (
-            <div className='container'>
-                <Popular />
-            </div>
+            <BrowserRouter>
+                <div className='container'>
+                    <Nav />
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route exact path='/battle' component={Battle} />
+                        <Route path='/popular' component={Popular} />
+                        <Route render={() => <p> Not Found </p>} />
+                    </Switch>   
+                </div>
+            </BrowserRouter>
+
         )
     }
 }
