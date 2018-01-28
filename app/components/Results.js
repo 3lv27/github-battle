4c791b406563b9ab159d02109f7a99b95db91cd1
3lv27 <elvin.gm27@gmail.com>
2018-01-28 23:33:21 +0100

    progress: more stuff

---
+++ app/components/Results.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Results extends Component {
+    render() {
+        return(
+            <div>Results</div>
+        )
+    }
+
+}
+
+export default Results
